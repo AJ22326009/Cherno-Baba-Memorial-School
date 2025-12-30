06dc981c340d1e6dd6bd7d69358497755c06e247
aj22326009 <aj22326009@utg.edu.gm>
2025-12-30 12:43:17 +0000

    auth routes added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
 // Load environment variables
 dotenv.config();
 connectDB();
+
+//use json
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
