2ff86edf56a5c759d6441c899386b3a25a2eced2
aj22326009 <aj22326009@utg.edu.gm>
2025-12-30 12:38:17 +0000

    register and login controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+const User=require('../models/User.model');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+const registerUser=async(req,res)=>{
+    try {
+        const {name,email,password,role}=req.body;
+        const hashedPassword=await bcrypt.hash(password,10);
+        const newUser=new User({
+            name,
+            email,
+            password:hashedPassword,
+            role,
+        });
+        await newUser.save();
+        res.status(201).json({message:'User registered successfully'});
+    } catch (error) {
+        res.status(500).json({message:'User not registered',error:error.message});
+    }
+}
+
+const loginUser=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return  res.status(400).json({message:'Invalid email'});
+        }
+
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({message:'Invalid password'});
+        }
+
+        const accessToken=jwt.sign({
+            userId:user._id,
+            name:user.name,
+            role:user.role,
+            email:user.email
+        },
+            process.env.JWT_SECRET,
+            {expiresIn:'1h'}
+        );
+        
+        res.status(200).json({accessToken});
+    } catch (error) {
+        res.status(500).json({message:'Login failed',error:error.message});
+    }
+}
+
+module.exports={registerUser,loginUser};
