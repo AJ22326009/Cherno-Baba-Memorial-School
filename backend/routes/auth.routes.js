95baa883eac770a1d0d0fb919a807e371d8110dc
aj22326009 <aj22326009@utg.edu.gm>
2025-12-30 12:39:29 +0000

    register and login routes

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const {registerUser,loginUser}=require('../controllers/auth.controller');
+
+const authenticate=require('../middleware/authenticate.middleware');
+const authorize=require('../middleware/authorize.middleware');
+
+const router=express.Router();
+
+router.post('/register', authenticate, authorize('admin'), registerUser);
+router.post('/login',loginUser);
+
+module.exports=router;
