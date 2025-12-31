bbdbc15631f13bbfdd3510fa2a70faa6df25d89e
aj22326009 <aj22326009@utg.edu.gm>
2025-12-31 12:08:20 +0000

    classTeacherOnly and classTeacherAndAdmin authorizations

--- backend/middleware/authorize.middleware.js
+++ backend/middleware/authorize.middleware.js
@@ -27,4 +27,32 @@ const onlyParticularUser= (req, res, next) => {
     next();
 }
 
-module.exports={authorize, onlyParticularUser};
+const classTeacherAndAdmin=(req,res,next)=>{
+    if(!req.user){
+        return res.status(401).json({ message: 'user not logged in' });
+    }
+
+    const assignedClass=req.user.assignedClass;
+
+    if(req.body.classLevel !== assignedClass && req.user.role==='teacher'){
+        return res.status(403).json({ message: 'forbidden access: You can only do this to your assigned class' });
+    }
+
+    next();
+}
+
+const classTeacherOnly=(req,res,next)=>{
+    if(!req.user){
+        return res.status(401).json({ message: 'user not logged in' });
+    }
+
+    const assignedClass=req.user.assignedClass;
+
+    if(req.body.classLevel !== assignedClass){
+        return res.status(403).json({ message: 'forbidden access: You can only do this to your assigned class' });
+    }
+
+    next();
+}
+
+module.exports={authorize, onlyParticularUser, classTeacherAndAdmin, classTeacherOnly};
