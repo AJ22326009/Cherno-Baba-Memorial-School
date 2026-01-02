c3ae48d369b12b2c4266bf9f510cc5ef7e4f84d8
aj22326009 <aj22326009@utg.edu.gm>
2026-01-02 17:27:08 +0000

    user profile logic added

--- frontend/src/app/shared/user-profile/user-profile.ts
+++ frontend/src/app/shared/user-profile/user-profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './user-profile.css',
 })
 export class UserProfile {
+  user: any = null;
+  changePasswordMode: boolean = false;
+
+  constructor(private authService: AuthService) {
+    this.user = this.authService.getUser();
+  }
 
 }
