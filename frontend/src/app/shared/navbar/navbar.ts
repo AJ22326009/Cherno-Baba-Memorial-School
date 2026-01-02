f117685c1f6ca95b34ddeefc277330d05bb58253
aj22326009 <aj22326009@utg.edu.gm>
2026-01-02 17:26:38 +0000

    navBar logic added

--- frontend/src/app/shared/navbar/navbar.ts
+++ frontend/src/app/shared/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,26 @@ import { RouterLink } from "@angular/router";
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  user: any=null;
+  homeLink: string = '';
+  studentsLink: string = '/admin/students';
+  resultsLink: string = '/teacher/results';
+  usersLink: string = '/admin/users';
+  role: string = '';
 
+  constructor(private authService: AuthService) {
+    this.user=this.authService.getUser();
+    this.role=this.user?.role;
+    if(this.role==='admin'){
+      this.homeLink='/admin';
+    }else{
+      this.homeLink='/teacher';
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  
 }
