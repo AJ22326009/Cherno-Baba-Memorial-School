fcab51b0dcad826f115decf785c441ec63c2245e
aj22326009 <aj22326009@utg.edu.gm>
2026-01-02 17:21:39 +0000

    httpclient and interceptor added

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { interceptor } from './auth/interceptor/interceptor-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([interceptor])
+    )
   ]
 };
