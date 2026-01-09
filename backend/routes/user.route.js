326f5fa118832b3573d36f9ea27bbcb1859c9fff
aj22326009 <aj22326009@utg.edu.gm>
2026-01-09 20:51:15 +0000

    route for updating other user details

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,4 +1,4 @@
-const {getAllUsers, getUserById, deleteTeacher, updatePassword}=require('../controllers/User.controller');
+const {getAllUsers, getUserById, deleteTeacher, updatePassword, updateUser}=require('../controllers/User.controller');
 const {authorize, onlyParticularUser}=require('../middleware/authorize.middleware');
 
 const express=require('express');
@@ -15,5 +15,8 @@ router.delete('/:id', authorize('admin'), deleteTeacher);
 
 //route to update password
 router.patch('/:id/password', onlyParticularUser, updatePassword);
+
+//route for updating other user details
+router.put('/:id', authorize('admin'), updateUser);
 
 module.exports=router;
