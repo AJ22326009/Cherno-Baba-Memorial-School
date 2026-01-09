012e7a53595ccc5d901b73623ee7a1889a4e7231
aj22326009 <aj22326009@utg.edu.gm>
2026-01-09 20:45:29 +0000

    logic changed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,16 +4,15 @@ const jwt=require('jsonwebtoken');
 
 const registerUser=async(req,res)=>{
     try {
-        const {name,email,password,role, assignedClass, imageUrl}=req.body;
-        const hashedPassword=await bcrypt.hash(password,10);
-        const newUser=new User({
-            name,
-            email,
-            password:hashedPassword,
-            role,
-            assignedClass,
-            imageUrl
-        });
+        const userData=req.body;
+        const userExists=await User.findOne({email:userData.email});
+        if(userExists){
+            return res.status(400).json({message:'User already exists with this email'});
+        };
+
+        const hashedPassword=await bcrypt.hash(userData.password,10);
+        userData.password=hashedPassword;
+        const newUser=new User(userData);
         await newUser.save();
         res.status(201).json({message:'User registered successfully'});
     } catch (error) {
