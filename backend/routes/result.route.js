fc9d4ab0037ada2f68fb611f38d54f5a895f5d1a
aj22326009 <aj22326009@utg.edu.gm>
2026-01-10 23:19:02 +0000

    result route refined

--- backend/routes/result.route.js
+++ backend/routes/result.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {authorize, classTeacherOnly}=require('../middleware/authorize.middleware');
 
-const {createResult, generateClassPositions, getResultsByStudent, getResultsByClassTerm, getAllResults} = require('../controllers/result.controller');
+const {createResult, generateClassPositions, searchResults, getResultsByClass, deleteResult, editResult} = require('../controllers/result.controller');
 
 //create a new result
 router.post('/', authorize('teacher'),createResult);
@@ -10,13 +10,15 @@ router.post('/', authorize('teacher'),createResult);
 //generate class positions for a particular class and term
 router.post('/positions', authorize('teacher'), classTeacherOnly, generateClassPositions);
 
-//get all results of a particular student (authorization should be handled in controller)
-router.get('/student/:studentId', getResultsByStudent);
-
-//get all results of a particular class and term (authorization should be handled in controller)
-router.get('/class',getResultsByClassTerm);
-
 //get all results [only admin]
-router.get('/', authorize('admin'), getAllResults);
+router.get('/search/:name', authorize('admin'), searchResults);
+
+//get results in a class
+router.get('/class/:classLevel', getResultsByClass);
+
+router.delete('/:id', deleteResult);
+
+//edit a result
+router.put('/:id', editResult);
 
 module.exports = router;
