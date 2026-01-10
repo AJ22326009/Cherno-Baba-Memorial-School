4470f28bfec4206e5dacad924dd8e4bbcb64b6c5
aj22326009 <aj22326009@utg.edu.gm>
2026-01-10 23:29:59 +0000

    admin dashboard component updated

--- frontend/src/app/admin/dashboard/dashboard.ts
+++ frontend/src/app/admin/dashboard/dashboard.ts
@@ -4,10 +4,13 @@ import { UserService } from '../../shared/services/user.services/user';
 import { StudentService } from '../../shared/services/student.services/student';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { ResultService } from '../../shared/services/result.services/result';
+import { Validators, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { NgxPrintModule } from 'ngx-print';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [DatePipe],
+  imports: [DatePipe, ReactiveFormsModule, NgxPrintModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
@@ -18,8 +21,28 @@ export class AdminDashboard implements OnInit {
   maleStudents: number = 0;
   femaleStudents: number = 0;
   students: any[] = [];
+  teachers: any[] = [];
   latestStudents: any[] = [];
-  constructor(private authService: AuthService, private userService: UserService, private studentService: StudentService, private router: Router) {}
+
+  error: string = '';
+  success: string = '';
+  loadingSearch: boolean = false;
+  loadingDelete: boolean = false;
+  deleteResultId: string = '';
+
+  results: any[] = [];
+  noResults: boolean = false;
+
+  searchForm: FormGroup;
+
+  
+  constructor(private authService: AuthService, private userService: UserService, private studentService: StudentService, private router: Router, private resultService: ResultService, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      name: ['', Validators.required],
+      term: ['', Validators.required],
+      classLevel: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.user = this.authService.getUser();
@@ -29,7 +52,8 @@ export class AdminDashboard implements OnInit {
 
   countTeachers(){
     this.userService.getAllUsers().subscribe(users => {
-      this.teacherCount = users.filter((user: any) => user.role === 'teacher').length;
+      this.teachers = users.filter((user: any) => user.role === 'teacher');
+      this.teacherCount = this.teachers.length;
     });
   }
 
@@ -46,6 +70,61 @@ export class AdminDashboard implements OnInit {
       this.students = students;
       this.countStudents();
     });
+  }
+
+  searchResults(){
+    this.loadingSearch = true;
+    const { name, term, classLevel } = this.searchForm.value;
+    this.resultService.searchResults(name, term, classLevel).subscribe({
+      next: (results) => {
+        this.loadingSearch = false;
+        this.results = results;
+        this.noResults = this.results.length === 0;
+        
+      },
+      error: (error) => {
+        this.loadingSearch = false;
+        this.error = 'error searching results. Please try again later.';
+        setTimeout(() => {
+          this.error = '';
+        }, 3000);
+      }
+    });
+  }
+
+  deleteResult(resultId: string){
+    this.loadingDelete=true;
+    this.deleteResultId=resultId;
+    if(confirm('Are you sure you want to delete this result?')) {
+    this.resultService.deleteResult(resultId).subscribe({
+      next: () => {
+        this.loadingDelete=false;
+        console.log('Result deleted successfully');
+        this.results = this.results.filter(result => result._id !== resultId);
+      },
+      error: (err) => {
+        this.loadingDelete=false;
+        console.log('Error deleting result:', err);
+      }
+    });
+  }
+  }
+
+  clearSearch(){
+    this.results = [];
+    this.searchForm.get('name')?.setValue('');
+    this.searchForm.get('term')?.setValue('');
+    this.searchForm.get('classLevel')?.setValue('');
+    this.noResults = false;
+  }
+
+  findClassTeacher(classLevel: string): string {
+    const classTeacher = this.teachers.find(teacher => teacher.assignedClass === classLevel);
+    return classTeacher ? classTeacher.name : 'not assigned';
+  }
+
+  calStudentsInClass(classLevel: string): number {
+    return this.students.filter(student => student.classLevel === classLevel).length;
   }
 
   navigateToRegisterStudent(){
