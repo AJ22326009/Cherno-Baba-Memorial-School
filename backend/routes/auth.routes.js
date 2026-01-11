d559b208420a8cd3a280abcc941acdc1c5cfbcb1
aj22326009 <aj22326009@utg.edu.gm>
2026-01-11 11:29:52 +0000

    refresh and logout routes added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const {registerUser,loginUser}=require('../controllers/auth.controller');
+const {registerUser,loginUser, refreshUser, logoutUser}=require('../controllers/auth.controller');
 
 const authenticate=require('../middleware/authenticate.middleware');
 const {authorize}=require('../middleware/authorize.middleware');
@@ -8,5 +8,7 @@ const router=express.Router();
 
 router.post('/register', authenticate, authorize('admin'), registerUser);
 router.post('/login',loginUser);
+router.post('/refresh',refreshUser);
+router.post('/logout',authenticate, logoutUser);
 
 module.exports=router;
