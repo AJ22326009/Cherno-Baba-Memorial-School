492d06823f33340f37e52b06e128b076f6871fc9
aj22326009 <aj22326009@utg.edu.gm>
2026-01-11 11:35:07 +0000

    interceptor updated to refresh when access token has expired and resfresh token hasn't

--- frontend/src/app/auth/interceptor/interceptor-interceptor.ts
+++ frontend/src/app/auth/interceptor/interceptor-interceptor.ts
@@ -1,16 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import { catchError, switchMap, throwError } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
 
 export const interceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
   const authService = inject(AuthService);
-
   const accessToken = localStorage.getItem('accessToken');
 
-  if(!req.url.includes('/auth/login')){
     if(accessToken){
       req = req.clone({
         setHeaders: {
@@ -18,23 +16,37 @@ export const interceptor: HttpInterceptorFn = (req, next) => {
         }
       });
     }
-  }
 
   return next(req).pipe(
-    catchError((error)=>{
-      if(error.status === 401 || error.status === 403){
-        authService.logout();
+    catchError((error: HttpErrorResponse)=>{
+      const isLoginReq = req.url.includes('/auth/login');
+      //case 1: token expired or invalid
+      if(error.status === 401 && !isLoginReq){
+        //try to refresh token
+        return authService.refresh().pipe(
+          switchMap((res: any)=>{
+            localStorage.setItem('accessToken', res.accessToken);
+
+            const retryReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${res.accessToken}`
+              }
+            })
+            return next(retryReq);
+          }),
+
+          //if refresh also fails, logout user
+          catchError((err)=>{
+            authService.logout();
+            return throwError(()=>err);
+          })
+        )
       }
 
-      // if(error.status === 0){
-      //   router.navigate(['/error'],{ queryParams: { reason: 'Server Down' } });
-      //   console.log('Server is down');
-      // }
-
-      // if(error.status === 500){
-      //   router.navigate(['/error'],{ queryParams: { reason: 'Server Error' } });
-      //   console.log('Internal Server Error');
-      // }
+      if(error.status === 0){
+        router.navigate(['/error'],{ queryParams: { reason: 'Server Down' } });
+        console.log('Server is down');
+      }
 
       return throwError(()=>error);
     })
